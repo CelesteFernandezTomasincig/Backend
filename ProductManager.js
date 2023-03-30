a2f688ad891daff72305c3f1609c1b8d6f77f2cb
Celeste Fernandez Tomasincig <celesteft00@gmail.com>
2023-03-30 09:45:32 -0300

    Segundo Entregable

--- ProductManager.js
+++ ProductManager.js
@@ -1,98 +1,48 @@
+const fs = require('fs');
+
 class ProductManager {
-  constructor() {
-    this.products = []; // arreglo vacío para almacenar los productos
-    this.lastId = 0; // inicializamos el id autoincrementable
+  constructor(path) {
+    this.path = path;
   }
 
-  addProduct(title, description, price, thumbnail, code, stock) {
-    // Verificamos que todos los campos sean obligatorios
-    if (!title || !description || !price || !thumbnail || !code || !stock) {
-      throw new Error("Todos los campos son obligatorios.");
-    }
-
-    // Verificamos que el campo "code" no se repita en otros productos
-    const productExists = this.products.some(product => product.code === code);
-    if (productExists) {
-      throw new Error("El código del producto ya existe.");
-    }
-
-    // Creamos el objeto "product" con los datos proporcionados y el id autoincrementable
-    const product = {
-      id: ++this.lastId,
-      title,
-      description,
-      price,
-      thumbnail,
-      code,
-      stock
-    };
-
-    // Agregamos el objeto "product" al arreglo "products"
-    this.products.push(product);
-  }
-
-  removeProduct(id) {
-    const index = this.products.findIndex(product => product.id === id);
-    if (index > -1) {
-      this.products.splice(index, 1);
-    }
-  }
-
-  findProductById(id) {
-    return this.products.find(product => product.id === id);
+  addProduct(product) {
+    const products = this.getProducts();
+    product.id = products.length + 1;
+    products.push(product);
+    fs.writeFileSync(this.path, JSON.stringify(products));
   }
 
   getProducts() {
-    return this.products;
+    if (!fs.existsSync(this.path)) {
+      return [];
+    }
+    const data = fs.readFileSync(this.path, 'utf-8');
+    return JSON.parse(data);
   }
 
   getProductById(id) {
-    const product = this.findProductById(id);
-    if (!product) {
-      console.error("Not found");
-    }
-    return product;
+    const products = this.getProducts();
+    return products.find((product) => product.id === id);
   }
 
-  updateProduct(id, fieldToUpdate) {
-    const product = this.findProductById(id);
-    if (!product) {
-      console.error("Not found");
-      return;
+  updateProduct(id, updatedProduct) {
+    const products = this.getProducts();
+    const index = products.findIndex((product) => product.id === id);
+    if (index !== -1) {
+      products[index] = { ...updatedProduct, id };
+      fs.writeFileSync(this.path, JSON.stringify(products));
     }
-    Object.assign(product, fieldToUpdate);
   }
 
   deleteProduct(id) {
-    this.removeProduct(id);
+    const products = this.getProducts();
+    const updatedProducts = products.filter((product) => product.id !== id);
+    fs.writeFileSync(this.path, JSON.stringify(updatedProducts));
   }
 }
 
-const productManager = new ProductManager();
-
-// Agregamos el primer producto
-productManager.addProduct("Zapatilla", "Zapatillas talle 37", 10.99, "img-1", "COD001", 50);
-
-// Agregamos el segundo producto
-productManager.addProduct("Reloj", "Reloj Rolex", 25.99, "img-2", "COD002", 25);
-
-// Agregamos el tercer producto
-productManager.addProduct("Campera", "Campera de cuero", 5.99, "img-3", "COD003", 100);
-
-// Agregamos el cuarto producto
-productManager.addProduct("Camisa", "Camisa de Zara", 50.99, "img-4", "COD004", 10);
+const productManager = new ProductManager('./products.json');
 
 console.log(productManager.getProducts());
 
-// Actualizar el segundo producto
-productManager.updateProduct(2, {
-  price: 30.99,
-  stock: 20
-});
 
-console.log(productManager.getProducts());
-
-// Eliminar producto
-productManager.deleteProduct(2);
-
-console.log(productManager.getProducts());
